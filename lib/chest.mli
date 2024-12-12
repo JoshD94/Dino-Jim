ffc8d1dd407ea92f55940dbb4445ff0c4172eacc
Evan Navar Root <etn32@cornell.edu>
2024-12-11 21:14:57 -0500

    Document chest: - documentation for the chest module

--- lib/chest.mli
+++ lib/chest.mli
@@ -1,5 +1,13 @@
 type t
+(** The type of a chest *)
 
 val create_chest : unit -> t
+(** [create_chest ())] creates a chest the contains all the possible skins to be
+    obtainted from chests *)
+
 val more_skins : t -> bool
+(** [more_skins chest] returns true if [chest] has more skins to be obtained *)
+
 val open_chest : t -> Player.t -> unit
+(** [open_chest chest player] gets the next available skin from [chest] and
+    gives it to [player]. Does nothing if [chest] has no more skins *)
